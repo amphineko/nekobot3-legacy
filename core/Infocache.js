4f65c2ba2f0ab7f823305e3ac3cd07e552d1d5bb
椎名奈緒 <sukareki@gmail.com>
2014-03-30 01:52:59 +0800

    0.1 / 修补了Infocache的群信息处理函数

--- core/Infocache.js
+++ core/Infocache.js
@@ -1,4 +1,11 @@
-﻿var api = require('./qqapi2');
+﻿/*
+	Nekobot v2 / Core
+	
+	@package  pw.futa.nekobot.core.userinfo
+	@author   Amphineko (Naoki Rinmous)
+*/
+
+var api = require('./qqapi2');
 var fs = require('fs'); // For JSON output test
 var log = new (require('Log'))('debug');
 
@@ -141,23 +148,28 @@ function refreshGroupInfo(gcode, callback) {
 		if (!error)
 			if (ret)
 				if (ret.retcode === 0) {
-					var res = ret.result;
-					var members = {};
+					try {
+						var res = ret.result;
+						var members = {};
 
-					res.minfo.filter(function (element) {
-						members[element.uin] = element;
-						members[element.uin].rawnick = members[element.uin].nick;
-					});
-					if (res.cards)
-						res.cards.filter(function (element) {
-							members[element.muin].nick = element.card;
+						res.minfo.filter(function (element) {
+							members[element.uin] = element;
+							members[element.uin].nick = members[element.uin].rawnick = members[element.uin].nick;
 						});
+						if (res.cards)
+							res.cards.filter(function (element) {
+								members[element.muin].nick = element.card;
+							});
 
-					listGroupInfo[gcode].info = res.ginfo;
-					listGroupInfo[gcode].members = members;
+						listGroupInfo[gcode].info = res.ginfo;
+						listGroupInfo[gcode].members = members;
 
-					log.info('<Infocache> 讀取群組資料完成: ' + res.ginfo.name + ' (' + gcode + ')');
-					callback2(true, null);
+						log.info('<Infocache> 讀取群組資料完成: ' + res.ginfo.name + ' (' + gcode + ')');
+						callback2(true, null);
+					} catch (error) {
+						log.info('<Infocache> 處理群組資料時遇到錯誤: ' + error);
+						callback2(false, 'process group info error: ' + error);
+					}
 				} else
 					callback2(false, 'retcode != 0 [' + ret.retcode + ']');
 			else
