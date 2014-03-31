653848d7be4d8bae74fd648dca2b2b6dafc6002a
椎名奈緒 <sukareki@gmail.com>
2014-04-01 00:41:40 +0800

    0.2 / 开始重写了喵~ (´･ω･‘)

--- Bootstrap.js
+++
@@ -1,9 +0,0 @@
-/*
-	Nekobot v2 / Core
-	
-	@package  pw.futa.nekobot.bootloader.call
-	@author   Amphineko (Naoki Rinmous)
-*/
-
-process.title = 'Nekobot v2 Console';
-require('./Bootloader').boot();
--- Readme.md
+++
@@ -1,4 +0,0 @@
-- Rename Config.yaml.sample to Config.yaml, modify it with your account.
-- npm install & node Bootstrap
-
-That's all, have fun. :)
